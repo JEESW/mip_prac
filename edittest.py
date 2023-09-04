ec5359880ff9b95a8b454c29c6a0c5d7c727d856
JEESW <jeesw1221@gmail.com>
2023-09-04 17:45:31 +0900

    add file mod

---
+++ edittest.py
@@ -0,0 +1,21 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+
+print(b)
+
+print(b, a)
+
+print("python", a, b)
+
+number_school = 2394584903  # 정수형 변수
+str_name = "python"         # 문자열 변수
+bool_view = False           # boolean 변수
+flt_pi = 3.14               # 실수형 변수
+char_element = 'a'          # 문자형 변수
+
+chars = "hello world"
+
+print("number : ", number_school, " name : ", str_name, " bool : ", bool_view, " float : ", flt_pi, " charcter : ", char_element, " charcters : ", chars)
