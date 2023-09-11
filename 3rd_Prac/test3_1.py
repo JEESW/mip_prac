ba8be8f70816c98733839f4a4fe5840b57b1d014
JEESW <jeesw1221@gmail.com>
2023-09-11 17:21:54 +0900

    commit

---
+++ 3rd_Prac/test3_1.py
@@ -0,0 +1,52 @@
+my_set = {5, 8, 3, 7, 1, "h"}
+""" my_set = set([5, 8, 3, 7, 1, "h"])
+print(my_set)
+set_tmp = set("hello")
+print(set_tmp) """
+
+set_item = {7, 8, 4, 2, "h"}
+
+""" # print(my_set | set_item)
+print(my_set.union(set_item))
+# print(my_set - set_item)
+print(my_set.difference(set_item))
+#print(my_set & set_item)
+print(my_set.intersection(set_item)) """
+
+print(my_set)
+
+""" my_set.add(9)
+print(my_set) """
+
+""" my_set.update([5, 9, 7, 4])
+print(my_set) """
+
+# my_set.clear()
+
+# my_set.remove(5)
+
+""" my_set.discard(2)
+print(my_set) """
+
+""" my_set.difference_update(set_item)
+print(my_set) """
+
+my_int = 10
+print(my_int)
+
+print(my_int + 10)
+my_str = str(my_int)
+
+print(my_str)
+
+print(my_str + "hello")
+
+my_str = '10'
+print(my_str)
+my_int = int(my_str)
+print(my_int)
+
+print(my_int + 10)
+
+""" my_int2 = int("ten")
+print(my_int2) """
---
+++ 3rd_Prac/test3_2.py
@@ -0,0 +1,63 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+
+a = 2
+print(a)
+a += 2
+print(a)
+a -= 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a **= 2
+print(a)
+
+a = 10
+b = 4
+
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+a = 1
+b = 0
+
+x = True
+y = False
+
+print(a and b)
+print(x and y)
+
+print(a or b)
+print(x or y)
+
+print(not a)
+print(not b)
+print(not x)
+print(not y)
+
+a = 10
+b = 3
+
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(~b)
+print(a << 2)
+print(b << 2)
+print(b >> 1)
+print(b >> 1)
